809ce259fd3045693d55764838311eccae7acd4b
birkan <b.561birkan@gmail.com>
2022-06-06 17:04:05 +0300

    page is dynamically working

---
+++ event.js
@@ -0,0 +1,60 @@
+const hold = Math.floor(Math.random() * 100);
+// console.log(number);
+const buton = document.getElementById("btn");
+const guess = document.getElementById("guess");
+const write = document.getElementById("write");
+let count = document.getElementById("count");
+const quit = document.getElementById("quit");
+const again = document.getElementById("again");
+const input = document.getElementById("input");
+
+let forCount = 0;
+quit.onclick = function(){
+    document.querySelector(".container").style.display = "none";
+    document.getElementById("quit1").style.display = "flex";
+}
+
+
+buton.onclick = function(){
+    let number = document.getElementById("input").value;
+    number = Number(number)
+    if(number >= 100 || number <= 0){
+        alert("Please enter a number between 1-100");
+    }
+    else{
+        if(hold > number){
+            write.innerHTML = `A bit higher than ${number}`;
+            input.value = " ";
+            guess.innerHTML = `Guess The Number (${number}-100)`;
+            forCount++
+            count.innerHTML = `Number of attempts: ${forCount} `;
+
+            // console.log(write);
+        } else if (hold < number){
+            write.innerHTML = `A bit lower than ${number}
+            `;
+            guess.innerHTML = `Guess The Number (1-${number})`;
+            input.value = " ";
+
+            forCount++
+            count.innerHTML = `Number of attempts: ${forCount}`;
+
+        }
+        else{
+            document.querySelector(".container").style.display = "none";
+            document.getElementById("win").style.display = "flex";
+        }
+    }
+}
+again.onclick = function(){
+    document.querySelector(".container").style.display = "flex";
+    document.getElementById("win").style.display = "none";
+}
+window.onload = () => {
+    input.focus();
+}
+input.addEventListener(("keydown", (e) => {
+    if(e.keyCode === 13){
+        buton.onclick();
+    }
+}))
